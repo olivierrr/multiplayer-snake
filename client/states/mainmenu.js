466f998395d67977827ed6eda3cbb81029cf68d0
olivierrr <oliviersinbox@gmail.com>
2014-11-28 15:53:44 -0800

    mainmenu

--- client/states/mainmenu.js
+++ client/states/mainmenu.js
@@ -3,27 +3,19 @@ module.exports = function (states) {
 
 	var state = {}
 
-	// temp buttons until danecando does things
+	var elem = document.getElementById('home-menu')
+	var btn1 = document.getElementById('singleplayer-btn')
+	var btn2 = document.getElementById('multiplayer-btn')
+
+	btn1.addEventListener('click', states.go.bind(states, 'singleplayer'))
+	btn2.addEventListener('click', states.go.bind(states, 'multiplayer'))
+
 	state.create = function () {
-
-		var btn1 = document.createElement('button')
-		var btn2 = document.createElement('button')
-
-		btn1.innerHTML = 'singleplayer'
-		btn2.innerHTML = 'multiplayer'
-
-		document.body.appendChild(btn1)
-		document.body.appendChild(btn2)
-
-		btn1.addEventListener('click', states.go.bind(states, 'singleplayer'))
-		btn2.addEventListener('click', states.go.bind(states, 'multiplayer'))
-
+		elem.className = ''
 	}
 
 	state.destroy = function () {
-
-		document.body.innerHTML = ''
-
+		elem.className = 'hidden'
 	}
 
 	return state
