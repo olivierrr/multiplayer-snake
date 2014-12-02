10baf95cccc9fb855fdc372eed98431e89da2e76
olivierrr <oliviersinbox@gmail.com>
2014-12-01 23:58:07 -0800

    create room button

--- client/lobby.js
+++ client/lobby.js
@@ -7,6 +7,17 @@ module.exports = {
 
 var $lobby = document.querySelector('#lobby-area') 
 var $roomList = $lobby.querySelector('.room-list')
+var $createRoom = $lobby.querySelector('#create-room-btn')
+
+$createRoom.addEventListener('click', function () {
+  smoke.prompt('new room name', function(input){
+    if (input) cloak.message('createRoom', {roomName: input})
+  }, {
+    ok: "create",
+    cancel: "cancel",
+    reverseButtons: true
+  });
+})
 
 function render (rooms) {
   $roomList.innerHTML = ''
